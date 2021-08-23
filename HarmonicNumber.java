ebd51ae25a62a8bc89aece736d9c1ac6b7fe4277
saurabh <dekapurwars@gmail.com>
2021-08-24 00:37:38 +0530

    printing the nth harmonic output value

---
+++ HarmonicNumber.java
@@ -0,0 +1,18 @@
+
+	import java.util.Scanner;
+	public class HarmonicNumber {
+	public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	int N;
+	double result=0.0;
+	System.out.println("Enter the Nth Harmonic Number");
+	N = s.nextInt();
+	while (N>0) {
+	result =result+ (double)1/N;
+	N--;
+	}
+	System.out.println("Nth harmonic output is= "+result);
+	}
+	}
+
+
