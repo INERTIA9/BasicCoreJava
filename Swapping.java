b7f7f25472872898aa7fd12b04055b474b187580
saurabh <dekapurwars@gmail.com>
2021-08-24 00:45:29 +0530

    Swapping of Numbers

---
+++ Swapping.java
@@ -0,0 +1,16 @@
+
+	import java.util.*;
+	public class Swapping {
+	public static void main(String[] args) {
+	Scanner s  = new Scanner(System.in);
+	System.out.println("Enter the number you need to swap: a;b");
+	double a = s.nextDouble();
+	double b = s.nextDouble();
+	double t;
+	System.out.println("numbers before swapping a = "+a + "b = "+b);
+	t=a;
+	a=b;
+	b=a;
+	System.out.println("after swapping the numbers are a= "+b +"b= "+t);
+	}
+	}
